67d6dff05fbb674fac7937565613270305e19d76
ALessandro De Angelis <alessandrodea22@gmail.com>
2015-09-07 17:11:25 +0200

    Added code for raspberry pi 2

--- x/views.py
+++ x/views.py
@@ -1,5 +1,4 @@
-from django.core.urlresolvers import reverse
-from django.shortcuts import render, redirect
+import RPi.GPIO as GPIO
 
 # Create your views here.
 from django.views.generic import View, TemplateView
@@ -19,5 +18,12 @@ def toggle_light(value):
     if value == 'ON':
         # Comandi per accendere la luce
         print("Light on")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.OUT)
+        GPIO.output(4, GPIO.HIGH)
+
     elif value == 'OFF':
         print("Light off")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.OUT)
+        GPIO.output(4, GPIO.LOW)
