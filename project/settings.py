f4f26df607fb17de12bb6d683c4d9f4a40be4083
ALessandro De Angelis <alessandrodea22@gmail.com>
2015-09-15 11:36:42 +0200

    modified template

--- project/settings.py
+++ project/settings.py
@@ -39,9 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'x',
     'rest_framework',
-    'material',
-    'material.frontend',
-    'material.admin',
 )
 
 MIDDLEWARE_CLASSES = (
