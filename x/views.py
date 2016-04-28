c489d3943a5e0a5a480c48eb9c52287fdeca5e68
Alessandro <alessandrodea22@gmail.com>
2016-04-28 16:32:50 +0200

    added custom.css, update requirements, conf_list modified

--- x/views.py
+++ x/views.py
@@ -38,12 +38,13 @@ def get_temp(pin, sensor, pk):
     import sys
     import Adafruit_DHT
 
-    termos = Temperature.objects.get(id=pk)
+    termos = Temperature.objects.get(id=1)
     humidity, temperature = Adafruit_DHT.read_retry(int(sensor), int(pin))
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         termos.temperature = temperature
         termos.Humidity = humidity
+        termos.save()
     else:
         print('Failed to get reading. Try again!')
         sys.exit(1)
