882733d482e6a7546d7c83ec49e41fb487756fd1
Alessandro De Angelis <alessandrodea22@gmail.com>
2016-11-28 20:01:06 +0100

    updated django middleware

--- django-domotic/project/settings/base.py
+++ django-domotic/project/settings/base.py
@@ -34,25 +34,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Application definition
 
 INSTALLED_APPS = (
+    'domotic',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'domotic',
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+
 )
 
 ROOT_URLCONF = 'project.urls'
